4c61d6d4f1e6d6a2a472d1b3e9b23f15c120bfe3
sarah <ssh2198@columbia.edu>
2024-06-21 04:28:25 -0400

    confused snapping

--- notebooks/scripts/MapVisualization.py
+++ notebooks/scripts/MapVisualization.py
@@ -1,88 +1,112 @@
 import folium
 from folium.plugins import HeatMap, HeatMapWithTime, MousePosition
 import pandas as pd
-
+import geopandas as gpd
 
 class MapVisualization:
-    def __init__(self):
-        # TODO: Bring out color/weight/opacity to be member variables
-        pass
+    def __init__(self, gps_df):
+        self.gps_df = gps_df
+        self.folium_map = folium.Map(location=[gps_df['lat'].mean(), 
+                                               gps_df['long'].mean()])
 
-    @staticmethod
-    def polyline(gps_df, new_coords):
+    def view_map(self):
+        MousePosition(position="topright").add_to(self.folium_map)
+        self.folium_map.fit_bounds([[self.gps_df['lat'].min(), self.gps_df['long'].min()], 
+                                    [self.gps_df['lat'].max(), self.gps_df['long'].max()]])
+        return self.folium_map
+
+
+    def polyline(self, new_coords):
         """
         Create a folium map with a polyline comparing the original GPS data (blue) 
         to some new filtered data (red).
 
         @param:
-            - gps_df: DataFrame containing the original GPS data and some new filtered data
             - new_coords: List of strings containing the column names of the new filtered data
         
         Currently comparing original coordinates to:
             - kalman filtered data
             - road snapped data
         """
-        folium_map = folium.Map(location=[gps_df['lat'].mean(), 
-                                          gps_df['long'].mean()])
         # Vanilla polyline
-        folium.PolyLine(locations=gps_df[['lat', 'long']], 
+        folium.PolyLine(locations=self.gps_df[['lat', 'long']], 
                         color="#3480eb",
                         weight=10, 
-                        tooltip="Original GPS data").add_to(folium_map)
+                        tooltip="Original GPS data").add_to(self.folium_map)
         
         # Filtered polyline
         new_lat, new_long = new_coords
-        folium.PolyLine(locations=gps_df[[new_lat, new_long]], 
+        folium.PolyLine(locations=self.gps_df[[new_lat, new_long]], 
                         color="#FF0000",
                         weight=3, 
-                        tooltip="Kalman filtered GPS data").add_to(folium_map)
-        
-        MousePosition(position="topright").add_to(folium_map)
-        folium_map.fit_bounds([[gps_df['lat'].min(), gps_df['long'].min()], 
-                               [gps_df['lat'].max(), gps_df['long'].max()]])
-        return folium_map
+                        tooltip="Kalman filtered GPS data").add_to(self.folium_map)
+    
 
-    @staticmethod
-    def heatmap(gps_df, new_coords):
+    def add_geojson_circles(self, new_coords):
+        """
+        Compare the original GPS data (blue) to some new filtered data (red) as hoverable GoeJSON circles
+        """
+
+        # Convert both vanilla and new pandas dataframes to geodataframes
+        new_lat, new_long = new_coords
+        vanilla_gdf = gpd.GeoDataFrame(self.gps_df, 
+                                       geometry=gpd.points_from_xy(self.gps_df['long'], 
+                                                                   self.gps_df['lat']), 
+                                       crs="EPSG:4326")
+        new_gdf = gpd.GeoDataFrame(self.gps_df, 
+                                   geometry=gpd.points_from_xy(self.gps_df[new_long], 
+                                                               self.gps_df[new_lat]), 
+                                   crs="EPSG:4326")
+
+        folium.GeoJson(
+            vanilla_gdf,
+            name=f'Person {self.gps_df["person"].iloc[0]}',
+            marker=folium.Circle(radius=10, fill_color="blue", fill_opacity=0.4, color="black", weight=1),
+            tooltip=folium.GeoJsonTooltip(fields=["person", "date", "time"]),
+            popup=folium.GeoJsonPopup(fields=["person", "date", "time"]),
+            highlight_function=lambda x: {"fillOpacity": 0.8},
+            zoom_on_click=True,
+        ).add_to(self.folium_map)
+
+        folium.GeoJson(
+            new_gdf,
+            name=f'Person {self.gps_df["person"].iloc[0]}',
+            marker=folium.Circle(radius=10, fill_color="red", fill_opacity=0.4, color="black", weight=1),
+            tooltip=folium.GeoJsonTooltip(fields=["person", "date", "time"]),
+            popup=folium.GeoJsonPopup(fields=["person", "date", "time"]),
+            highlight_function=lambda x: {"fillOpacity": 0.8},
+            zoom_on_click=True,
+        ).add_to(self.folium_map)
+       
+
+
+    def heatmap(self, new_coords):
         """
         Create a heatmap comparing the original GPS data (blue-cyan) to some new filtered data (yellow-red)
         @param:
-            - gps_df: DataFrame containing the original GPS data and some new filtered data
             - new_coords: List of strings containing the column names of the new filtered data
         """
-        # Create a folium map object
-        folium_map = folium.Map(location=[gps_df['lat'].mean(), 
-                                          gps_df['long'].mean()])
         
         # Vanilla heatmap
-        HeatMap(data=gps_df[['lat', 'long']], 
+        HeatMap(data=self.gps_df[['lat', 'long']], 
                 gradient={0.4: 'blue', 0.65: 'cyan'}, 
                 blur=15, 
                 radius=10
-        ).add_to(folium_map)
+        ).add_to(self.folium_map)
 
         # Filtered heatmap
         new_lat, new_long = new_coords
-        HeatMap(data=gps_df[[new_lat, new_long]], 
+        HeatMap(data=self.gps_df[[new_lat, new_long]], 
                 gradient={0.3: 'yellow', 0.65: 'red'}, 
                 blur=6, 
                 radius=5
-        ).add_to(folium_map)
-
-        MousePosition(position="topright").add_to(folium_map)
-        folium_map.fit_bounds([[gps_df['lat'].min(), gps_df['long'].min()], 
-                               [gps_df['lat'].max(), gps_df['long'].max()]])
-        return folium_map
+        ).add_to(self.folium_map)
 
 
-    @staticmethod
-    def animated_heatmap(gps_df, new_coords, animate_new_data=True):
-
-        # Create a folium map object
-        folium_map = folium.Map(location=[gps_df['lat'].mean(), gps_df['long'].mean()], zoom_start=13)
+    def animated_heatmap(self, new_coords, animate_new_data=True):
 
         # Convert 'cst_datetime' column to datetime
-        gps_df['cst_datetime'] = pd.to_datetime(gps_df['cst_datetime'])
+        self.gps_df['cst_datetime'] = pd.to_datetime(self.gps_df['cst_datetime'])
 
         # Initialize an empty list to store cumulative data
         data = []  
@@ -91,7 +115,7 @@ class MapVisualization:
         new_lat, new_long = new_coords
 
         # Transform each row into a list and accumulate
-        for _, row in gps_df.iterrows():
+        for _, row in self.gps_df.iterrows():
             current_data = [[row['lat'], row['long']]]
             current_filtered_data = [row[[new_lat, new_long]].tolist()]
             if data:
@@ -106,38 +130,32 @@ class MapVisualization:
 
         # Select which data to animate (sorry kind of repetitive)
         if animate_new_data is True:
-            HeatMap(data=gps_df[['lat', 'long']], 
+            HeatMap(data=self.gps_df[['lat', 'long']], 
                         gradient={0.4: 'blue', 0.65: 'cyan'}, 
                         min_opacity=0.01,
                         blur=6, 
                         radius=10,
-            ).add_to(folium_map)
+            ).add_to(self.folium_map)
             # Animate the new filtered data
             HeatMapWithTime(
                 data=filtered_data,
-                index=gps_df['cst_datetime'].dt.strftime('%Y-%m-%d %H:%M:%S').tolist(),
+                index=self.gps_df['cst_datetime'].dt.strftime('%Y-%m-%d %H:%M:%S').tolist(),
                 gradient={0.3: 'yellow', 0.65: 'red'}, 
                 radius=8,
                 display_index=True
-            ).add_to(folium_map)
+            ).add_to(self.folium_map)
         else:
             # Create HeatMapWithTime instance and add to the folium map
-            HeatMap(data=gps_df[[new_lat, new_long]], 
-                        gradient={0.3: 'yellow', 0.65: 'red'}, 
-                        min_opacity=0.01,
-                        blur=6, 
-                        radius=10,
-            ).add_to(folium_map)
+            HeatMap(data=self.gps_df[[new_lat, new_long]], 
+                    gradient={0.3: 'yellow', 0.65: 'red'}, 
+                    min_opacity=0.01,
+                    blur=6, 
+                    radius=10,
+            ).add_to(self.folium_map)
             HeatMapWithTime(
                 data=data,
-                index=gps_df['cst_datetime'].dt.strftime('%Y-%m-%d %H:%M:%S').tolist(),
+                index=self.gps_df['cst_datetime'].dt.strftime('%Y-%m-%d %H:%M:%S').tolist(),
                 gradient={0.4: 'blue', 0.65: 'cyan'}, 
                 radius=10,
                 display_index=True
-            ).add_to(folium_map)
-
-        MousePosition(position="topright").add_to(folium_map)
-        folium_map.fit_bounds([[gps_df['lat'].min(), gps_df['long'].min()], 
-                               [gps_df['lat'].max(), gps_df['long'].max()]])
-        
-        return folium_map
+            ).add_to(self.folium_map)
